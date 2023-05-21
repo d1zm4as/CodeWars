0a79183bb2799282b753cde0693e65966988fd28
d1zm4as <pedrofamilia364@gmail.com>
2023-05-21 13:06:48 -0300

    +2

---
+++ C/asciiTotal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int uni_total(const char *s) {
+    int result = 0;
+    for (int i = 0; i < (int)strlen(s); ++i)
+        result += (int)s[i];
+
+    return  result;
+}
+
+
+
+
+int main(){
+
+    char s[] = "GGG";
+    printf("%d\n",uni_total(s));
+    return 0;
+}
