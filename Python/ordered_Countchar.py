ebfbcc7c5bee35b6bc31f8133e5401320ac2da9a
d1zm4as <pedrofamilia364@gmail.com>
2023-06-17 13:37:25 -0300

    +1

--- Python/ordered_Countchar.py
+++ Python/ordered_Countchar.py
@@ -1,11 +1,22 @@
 def func(x):
-    return x[1],[0]
+    return x[1]
  
 s = "abracadabra"
-copy = set(s)
-"""
-lista = sorted([(x,s.count(x))for x in copy],key = lambda a : a[1])
+# copy = set(s) # set nao mantem a ordem inicial
 
-print(lista[::-1])
-"""
+
+#lista = [(x,s.count(x))for x in copy]
+
+
+copy = []
+for x in s:
+    if x not in copy:
+        copy.append(x)
+
+print(copy)
+
+lista = [(x,s.count(x))for x in copy]
+print(lista)
+
+
 
