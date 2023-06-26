b2e7743590dae0c5d0eae51f11aa952cc9470e20
d1zm4as <pedrofamilia364@gmail.com>
2023-06-26 17:58:33 -0300

    +1

---
+++ Python/split_strings.py
@@ -0,0 +1,18 @@
+
+
+s = "abcd"
+
+size = len(s)
+lista = []
+if not s:
+    print("")
+
+if size%2!=0:
+
+    s+="_"
+    lista= [s[i:i+2] for i in range(0,size,2)]
+else:
+    lista= [s[i:i+2] for i in range(0,size,2)]
+
+
+print(lista)
