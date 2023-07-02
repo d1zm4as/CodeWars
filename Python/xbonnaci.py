8289112ce12ec7af04be0357224fbf9fff16c291
d1zm4as <pedrofamilia364@gmail.com>
2023-07-02 00:11:33 -0300

    +1

---
+++ Python/xbonnaci.py
@@ -0,0 +1,16 @@
+def Xbonacci(sigs,n):
+    cont= 0
+    lim = n
+    rep = n-len(sigs)
+    seq = sigs
+    if n==1:return [sigs[0]]
+    if n<=0:return []
+    if n< len(sigs):return sigs[:n]
+    for _ in range(rep):
+        soma = sum(seq[cont:lim])
+        seq.append(soma)
+        cont+=1
+        lim+=1
+    return seq
+
+ #basicament a mesma merda do tribonnaci mas so mudei o lim
