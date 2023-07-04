c0cd423029d85b610b63aaa925989a75c631d8dd
d1zm4as <pedrofamilia364@gmail.com>
2023-07-04 18:28:44 -0300

    +1

---
+++ Python/reversing_data.py
@@ -0,0 +1,15 @@
+data1 = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+data2 = [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
+
+
+lista = []
+
+size = 8
+arr = [data1[i:i + size] for i in range(0, len(data1), size)]
+
+for x in arr[::-1]:
+    for y in x:
+        lista.append(y)
+
+print(lista)
