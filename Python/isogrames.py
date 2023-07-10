1be696efcd52c1adb5bc5639157ec7a1352ab76b
d1zm4as <pedrofamilia364@gmail.com>
2023-07-09 21:19:57 -0300

    +1

--- Python/isogrames.py
+++
@@ -1,8 +0,0 @@
-ef is_isogram(string):
-    lista1 = set(x.lower() for x in string)
-    lista2 = [y for y in string]
-    
-    if len(lista1)==len(lista2):
-        return True
-    else:
-        return False
