a69e1ebf442cbf17b024e5151e31b4c59ed27950
d1zm4as <pedrofamilia364@gmail.com>
2023-07-09 22:31:34 -0300

    +1

--- Python/finding_d_missingletter.py
+++ Python/finding_d_missingletter.py
@@ -1,24 +1,14 @@
-abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def find_missing_letter(chars):
+    abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    pri = chars[0].lower()
+    cont = abc.index(pri)
+    lista = []
+    for _ in range(len(chars)):
+        lista.append(abc[cont])
 
+        cont+=1
 
-# chars = ['a','b','c','d','f']
-chars = ['O','Q','R','S']
-
-pri = chars[0].lower()
-cont = abc.index(pri)
-lista = []
-for _ in range(len(chars)):
-    lista.append(abc[cont])
-    
-    cont+=1
-print(lista)
-
-
-print(chars)
-print(lista)
-
-for x,y in zip(lista,chars):
-    cond = True if y.isupper() else False
-    if x!=y.lower():
-        print(x if not cond else x.upper())
-
+    for x,y in zip(lista,chars):
+        cond = True if y.isupper() else False
+        if x!=y.lower():
+            return x if not cond else x.upper()  
