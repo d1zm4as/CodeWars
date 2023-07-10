b83133a7e2b4df1c4fee3445a362293482cdef4f
d1zm4as <pedrofamilia364@gmail.com>
2023-07-10 12:57:10 -0300

    +1

--- Python/create_phone_number.py
+++ Python/create_phone_number.py
@@ -1,13 +1,18 @@
-def create_phone_number(n):
-    arr = [str(x) for x in n]
-    copy = ""
-    copy+="("
-    pri = "".join(arr[:3])
-    copy+=pri+")"+" "
+# def create_phone_number(n):
+    # arr = [str(x) for x in n]
+    # copy = ""
+    # copy+="("
+    # pri = "".join(arr[:3])
+    # copy+=pri+")"+" "
     
-    mid = "".join(arr[3:6])
-    copy+=mid+"-"
-    fim = "".join(arr[6:])
-    copy+=fim
-    return copy
+    # mid = "".join(arr[3:6])
+    # copy+=mid+"-"
+    # fim = "".join(arr[6:])
+    # copy+=fim
+	# return "({}{}{}) {}{}{}-{}{}{}{}".format(n)
 
+
+def create_phone_number(n):
+	return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+n = [0,1,2,3,4,5,6,7,8,9]
+print(create_phone_number(n))
