8a9c6498411697d2cca1f69ce2ee4001669cb509
d1zm4as <pedrofamilia364@gmail.com>
2023-07-10 19:04:46 -0300

    +1

--- Python/dividersPrimers.py
+++ Python/dividersPrimers.py
@@ -1,19 +1,15 @@
-
-values  = [5,7,11]
-
-powers = [2,1,1]
-
-total = []
-for x,y in zip(values,powers):
-        total.append(x**y)
+# Don't forget to sort your result list!
+def get_dividers(values, powers):
+    total = []
+    for x,y in zip(values,powers):
+            total.append(x**y)
 
 
-prod = 1
-for x in total:
-    prod*=x
+    prod = 1
+    for x in total:
+        prod*=x
 
-print(prod)
 
-lista = sorted([x for x in range(1,prod+1) if prod%x==0])
+    lista = sorted([x for x in range(1,prod+1) if prod%x==0])
 
-print(lista)
+    return lista
