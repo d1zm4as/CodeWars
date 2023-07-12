6141d8832e186d7ae29b4cd0e053395e99d33118
Pedr0 <64512195+d1zm4as@users.noreply.github.com>
2023-07-12 16:46:16 +0000

    +1

--- Python/merge_sort_arrays.py
+++ Python/merge_sort_arrays.py
@@ -1,2 +1,9 @@
-def merge_arrays(arr1, arr2):
-    return sorted(set(arr1+set(arr2)))
+def merge_arrays(a, b):
+    out = []
+    for n in a+b:
+        if n in a and n in b:
+            if a.count(n) == b.count(n):
+                out.append(n)
+        else:
+            out.append(n)
+    return sorted(set(out))
