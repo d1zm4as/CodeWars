290a5223d4dfee8f28db5fe071b227d116cdb0cf
d1zm4as <pedrofamilia364@gmail.com>
2023-07-17 10:11:21 -0300

    +1

--- Python/min_array.py
+++ Python/min_array.py
@@ -1,26 +1,8 @@
 def min_sum(arr):
-    # Your code here
-    pass
+    arr = sorted(lista)
 
-
-
-#arr = sorted([5,4,2,3])
-
-#arr = sorted([12,6,10,26,3,24])
-arr = sorted([9,2,8,7,5,4,0,6])
-
-tam = len(arr)
-half= tam//2
-a = arr[:half]
-b = arr[half:tam][::-1]
-
-
-soma = 0
-
-for x,y in zip(arr[:half],arr[half:tam][::-1]):
-    
-    soma+= x*y
-
-print(soma) 
-
-print(sum([(x*y) for x,y in zip (arr[:half],arr[half:tam][::-1])]))
+    tam = len(arr)
+    half= tam//2
+    a = arr[:half]
+    b = arr[half:tam][::-1]
+    return sum([(x*y) for x,y in zip (arr[:half],arr[half:tam][::-1])])
