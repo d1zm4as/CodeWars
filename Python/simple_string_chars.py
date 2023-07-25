89f7cb82a7530775e444e213e8d2c567ea1fc767
Pedr0 <64512195+d1zm4as@users.noreply.github.com>
2023-07-25 01:24:45 -0300

    Update simple_string_chars.py

--- Python/simple_string_chars.py
+++ Python/simple_string_chars.py
@@ -1,27 +1,13 @@
-s = "*'&ABCDabcde12345"
-
-
-up = 0
-
-low = 0
-
-nums = 0
-
-xs = 0
-
-for x in s:
-    ref = ord(x)
-    if ref>=65 and ref <=90:
-        up+=1
-    elif ref>=97 and ref <=122:
-        low+=1
-    elif x.isdigit():
-        
-        nums+=1
-    else:
-        xs+=1
-
-
-lista = [up,low,nums,xs]
-
-print(lista)
+def solve(s): 
+    up = 0 
+    low = 0 
+    nums = 0 
+    xs = 0 
+    for x in s: 
+        ref = ord(x) 
+        if ref>=65 and ref <=90: up+=1 
+        elif ref>=97 and ref <=122: low+=1 
+        elif x.isdigit(): nums+=1 
+        else: xs+=1
+    lista = [up,low,nums,xs] 
+    return lista
