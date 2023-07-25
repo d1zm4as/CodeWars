29f09213c47de0bb54a35c79c21ee934a5ad9faa
d1zm4as <pedrofamilia364@gmail.com>
2023-07-25 18:07:05 -0300

    +1

---
+++ Python/squares_sequance.py
@@ -0,0 +1,10 @@
+def squares(x, n):
+    if n <=0:return []
+    atual = x
+    lista = [atual]
+    cont = 1
+    while cont<n:
+        atual *=atual
+        lista.append(atual)
+        cont+=1
+    return lista
