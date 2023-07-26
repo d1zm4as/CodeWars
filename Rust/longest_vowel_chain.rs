feef5755436e033e83d0f3ecb43567680f5b1a05
d1zm4as <pedrofamilia364@gmail.com>
2023-07-26 19:40:33 -0300

    +1

---
+++ Rust/longest_vowel_chain.rs
@@ -0,0 +1,39 @@
+fn is_vowel(c: char) -> bool {
+    match c {
+        'a' | 'A' | 'e' | 'E' | 'i' | 'I' | 'o' | 'O' | 'u' | 'U' => true,
+        _ => false,
+    }
+}
+
+
+fn longest_vowel_chain(s: &str) -> usize {
+    let mut maior = 0;
+    let mut cont = 0;
+    for x in s.chars(){
+        if is_vowel(x){
+            cont+=1;
+        } 
+        else {
+            if cont>maior{ 
+                maior = cont;
+            }
+            cont = 0 ;
+       
+        }   
+    }
+    if cont>maior{ 
+        maior = cont;
+    }
+
+    maior
+}
+
+
+
+fn main(){
+    let a = "eai";
+    println!("{}",is_vowel('e'));
+
+    println!("{}",longest_vowel_chain(a));
+
+}
