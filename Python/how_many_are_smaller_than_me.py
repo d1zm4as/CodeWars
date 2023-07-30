f82d05c9087f2ba11f611a9ba2e5ec8bf305ceda
d1zm4as <pedrofamilia364@gmail.com>
2023-07-30 11:35:08 -0300

    +1

--- Python/how_many_are_smaller_than_me.py
+++ Python/how_many_are_smaller_than_me.py
@@ -1,18 +1,20 @@
 arr = [5, 4, 3, 2, 1]
 arr =  [1, 2, 0]
-lista=  []
+
+def smaller(arr):
+    lista=  []
 
 
-cont = 0
+    cont = 0
 
-while cont<len(arr):
-    ref = arr[cont]
-    total =  0
-    for x in arr[cont+1:]:
-        if x<ref:
-            total+=1
-    lista.append(total)
-    cont+=1
+    while cont<len(arr):
+        ref = arr[cont]
+        total =  0
+        for x in arr[cont+1:]:
+            if x<ref:
+                total+=1
+        lista.append(total)
+        cont+=1
 
 
-print(lista)
+    return lista
