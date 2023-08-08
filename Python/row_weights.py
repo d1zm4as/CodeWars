6e3a5ca5f051156d3c0cd91e0167abd05251aea0
d1zm4as <pedrofamilia364@gmail.com>
2023-08-08 13:50:30 -0300

    +1

--- Python/row_weights.py
+++ Python/row_weights.py
@@ -1,16 +1,13 @@
+def row_weights(lista):
+    
+    soma1 = 0
 
+    soma2 = 0
 
+    for idx,x in enumerate(lista):
+        if idx%2==0:
+            soma1+=x
+        else:
+            soma2+=x
 
-lista = [70,58,75,34,91]
-soma1 = 0
-
-soma2 = 0
-
-for idx,x in enumerate(lista):
-    if idx%2==0:
-        soma1+=x
-    else:
-        soma2+=x
-
-print([soma1,soma2])
-
+    return (soma1,soma2)
