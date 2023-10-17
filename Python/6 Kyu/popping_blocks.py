90b4088b1b39bcfab2096594d3d54bae8176023f
d1zm4as <pedrofamilia364@gmail.com>
2023-10-17 14:17:07 -0300

    +1

---
+++ Python/6 Kyu/popping_blocks.py
@@ -0,0 +1,8 @@
+arr = ['ab', 'ab', 'cd', 'cx', 'B']# ['cd', 'cx', 'B']
+
+lista = []
+
+cont = 0
+atual = arr[0]
+for x in arr:
+                                                                                  
