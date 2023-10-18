a9adbc6508243ae07ea83087e96bba5d3469fbac
d1zm4as <pedrofamilia364@gmail.com>
2023-10-18 14:22:07 -0300

    ups

--- Python/6 Kyu/popping_blocks.py
+++
@@ -1,8 +0,0 @@
-arr = ['ab', 'ab', 'cd', 'cx', 'B']# ['cd', 'cx', 'B']
-
-lista = []
-
-cont = 0
-atual = arr[0]
-for x in arr:
-                                                                                  
