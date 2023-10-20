5f4e04f029a77c38be4f35d807e35584093e9ae2
d1zm4as <pedrofamilia364@gmail.com>
2023-10-20 15:02:24 -0300

    +1

--- Python/7 Kyu/dominant_arrays.py
+++ Python/7 Kyu/dominant_arrays.py
@@ -2,5 +2,16 @@ arr = [16,17,14,3,14,5,2]#[ 17,14,5,2]
 
 lista = []
 
-tam = len(lista)
+tam = len(arr)
 
+cont = 0
+
+while cont < tam:
+    atual  = arr[cont]
+    ref = max(arr[cont:])
+    if atual>=ref and atual not in lista:
+        lista.append(atual)
+    print(atual,"---",ref)
+    cont+=1
+
+print(lista)
