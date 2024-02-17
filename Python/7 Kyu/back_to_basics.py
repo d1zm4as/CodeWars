80db460143711e466c5febf9ef32deaa6f22b166
d1zm4as <pedrofamilia364@gmail.com>
2024-02-16 21:24:46 -0300

    +1

---
+++ Python/7 Kyu/back_to_basics.py
@@ -0,0 +1,11 @@
+def types(x):
+    if type(x) == int :
+        return "int"
+    
+    if type(x) == float :
+        return "float"
+    
+    if type(x) == str :
+        return "str"
+    
+    return "bool"
