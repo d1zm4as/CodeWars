5beccab4b74750a7c273b6d884859c71a25d05a3
d1zm4as <pedrofamilia364@gmail.com>
2024-03-20 23:36:19 -0300

    r4

---
+++ C/5 Kyu/maximum_subarray_sum.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+
+int max(a,b){
+  if( a>=b){
+    return a;
+  }else{
+    return b;
+  }
+}
+
+
+int maxSequence(const int* array, size_t n) {
+  
+    size_t i;
+    
+    int soma = 0;
+    
+    int maior = 0;
+  
+    for(i = 0; i<n;i++){
+      
+      soma = max(array[i],soma+array[i]);
+      maior = max(maior,soma);
+      
+      
+    }
+
+  
+  return maior;
+}
