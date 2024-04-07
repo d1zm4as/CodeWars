bf21e04a2b5a1534e7bf31a1b4aa50a453eb7701
d1zm4as <pedrofamilia364@gmail.com>
2024-04-07 17:25:44 -0300

    +1

---
+++ CPP/6 Kyu/is_a_number_prime.cpp
@@ -0,0 +1,16 @@
+include <cmath>
+bool isPrime(int x) {
+  if (x==2 || x == 3){
+      return true;
+    }
+    if (x<2 || x%2==0){
+      return false;
+    }
+    int lim = round(sqrt(x))+1;
+    for (int i = 3; i <= lim;i+=2){
+      if(x%i==0){
+        return false;
+      }
+    }
+    return true;
+}
