0e62fc9436009be50527d7e39c7ffb52b4c36880
d1zm4as <pedrofamilia364@gmail.com>
2024-04-07 18:20:53 -0300

    +1

--- Python/5 Kyu/not_very_secure.py
+++ Python/5 Kyu/not_very_secure.py
@@ -1,7 +1,4 @@
 def alphanumeric(password):
     if not password:
         return False
-    for x in password:
-        if not x.isalpha() and not x.isdigit():
-            return False
-    return True
+    return password.isalnum()
