c2f6bfbbb5487441d9f6e74a36271a5866ffeed6
d1zm4as <pedrofamilia364@gmail.com>
2024-04-13 20:49:25 -0300

    -1

--- Python/6 Kyu/longest_palidrome.py
+++ Python/6 Kyu/longest_palidrome.py
@@ -1,11 +1,21 @@
+def pali(s):
+    return s==s[::-1]
+
+
+s = "A"
+s = "Hannah"
+s = "xyz__a_/b0110//a_zyx"
 def longest_palindrome (s):
 
     maior = 0
 
-    for j in range(1, len(s)+1):
-        for i in range(j):
-            sub = s[i:j]
-            if sub == sub[::-1]:
-                maior = max(maior, len(sub))
+    copy = ""
 
+    for x in s.lower():
+        copy+=x
+        if pali(copy):
+            maior = max(len(copy),maior)
+        
     return maior
+
+print(longest_palindrome(s))
