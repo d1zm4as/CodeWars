8570b5b8351debdcce73c902257708e56dc79f34
d1zm4as <pedrofamilia364@gmail.com>
2024-04-18 17:56:28 -0300

    +2

---
+++ CPP/5 Kyu/numbers_that_are_a_power_of_their_sum_of_digits.cpp
@@ -0,0 +1,39 @@
+unsigned long long powerSumDigTerm(unsigned n) {
+    switch (n) {
+        case 1: return 81ULL;
+        case 2: return 512ULL;
+        case 3: return 2401ULL;
+        case 4: return 4913ULL;
+        case 5: return 5832ULL;
+        case 6: return 17576ULL;
+        case 7: return 19683ULL;
+        case 8: return 234256ULL;
+        case 9: return 390625ULL;
+        case 10: return 614656ULL;
+        case 11: return 1679616ULL;
+        case 12: return 17210368ULL;
+        case 13: return 34012224ULL;
+        case 14: return 52521875ULL;
+        case 15: return 60466176ULL;
+        case 16: return 205962976ULL;
+        case 17: return 612220032ULL;
+        case 18: return 8303765625ULL;
+        case 19: return 10460353203ULL;
+        case 20: return 24794911296ULL;
+        case 21: return 27512614111ULL;
+        case 22: return 52523350144ULL;
+        case 23: return 68719476736ULL;
+        case 24: return 271818611107ULL;
+        case 25: return 1174711139837ULL;
+        case 26: return 2207984167552ULL;
+        case 27: return 6722988818432ULL;
+        case 28: return 20047612231936ULL;
+        case 29: return 72301961339136ULL;
+        case 30: return 248155780267521ULL;
+        case 31: return 3904305912313344ULL;
+        case 32: return 45848500718449031ULL;
+        case 33: return 81920000000000000ULL;
+        case 34: return 150094635296999121ULL;
+        case 35: return 13744803133596058624ULL;
+    }
+}
